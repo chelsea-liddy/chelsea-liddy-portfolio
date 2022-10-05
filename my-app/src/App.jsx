31ab91a384eb3f6acdc318c945845312da9146f7
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-10-05 19:07:25 +1300

    sset up tailwind and clear out uncessessary files

---
+++ my-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary">Button</button>
+    </div>
+  )
+}
+
+export default App
