923c9ca51a9ec434318be4d6348e5437d2628cd4
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-10-07 08:59:55 +1300

    start on contact section

--- my-app/src/Contact.jsx
+++ my-app/src/Contact.jsx
@@ -1,5 +1,20 @@
 function Contact() {
-  return <div>Contact info goes here</div>
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Get in touch</h2>
+        <div className="card-actions justify-end">
+          <a
+            href="https://www.linkedin.com/in/chelsea-liddy/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btn btn-accent">LinkedIn</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
