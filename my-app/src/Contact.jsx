e18222b3afe1f578b09a4b7492e7702071497d65
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-10-18 15:06:12 +1300

    add email

--- my-app/src/Contact.jsx
+++ my-app/src/Contact.jsx
@@ -1,16 +1,21 @@
 function Contact() {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">Get in touch</h2>
-        <div className="card-actions justify-end">
-          <a
-            href="https://www.linkedin.com/in/chelsea-liddy/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="btn btn-accent">LinkedIn</button>
-          </a>
+    <div className="bg-base-200">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Get in touch</h2>
+          <div className="card-actions justify-end">
+            <a
+              href="https://www.linkedin.com/in/chelsea-liddy/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="btn btn-accent">LinkedIn</button>
+            </a>
+            <div className="btn btn-primary">
+              E-mail: chelsea.r.liddy@gmail.com
+            </div>
+          </div>
         </div>
       </div>
     </div>
